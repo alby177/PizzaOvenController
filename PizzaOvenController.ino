611fe8d21fca0400f9a771dab13825dbf2f42584
Alberto Gotta <gotta.alberto@gmail.com>
2020-09-06 01:34:52 +0200

    Added thermocouple management
    [ADD] Thermocouple temperature is acquired each loop
    [FIX] Improved state machine for temperatures setting

--- PizzaOvenController.ino
+++ PizzaOvenController.ino
@@ -1,5 +1,7 @@
 #include <LiquidCrystal.h>
+#include "max6675.h"
 
+// LCD constant variables
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 const int btnNone   = 0;
 const int btnRight  = 1;
@@ -8,6 +10,12 @@ const int btnDown   = 3;
 const int btnLeft   = 4;
 const int btnSelect = 5;
 
+// Thermocouple variables
+const int sckPin = 10;
+const int csPin = 11;
+const int soPin = 12;
+MAX6675 ktc(sckPin, csPin, soPin);
+
 enum AvailableStates {
   ReadTemp = 0,
   SetMinTemp,
@@ -15,19 +23,19 @@ enum AvailableStates {
 };
 
 AvailableStates currentState = AvailableStates::ReadTemp;
-int currentTemp = 0;
-int minTemp = 0;
-int maxTemp = 1024;
+float minTemp = 0;
+float maxTemp = 1024;
+float currentTemp = 0;
 
 int readLcdButtons()
 {
   int adc_key_in = analogRead(0);
 
-  if (adc_key_in < 50)   return btnRight;  
-  if (adc_key_in < 195)  return btnUp; 
-  if (adc_key_in < 380)  return btnDown; 
-  if (adc_key_in < 555)  return btnLeft; 
-  if (adc_key_in < 790)  return btnSelect;   
+  if (adc_key_in < 10)   return btnRight;  
+  if (adc_key_in < 120)  return btnUp; 
+  if (adc_key_in < 260)  return btnDown; 
+  if (adc_key_in < 420)  return btnLeft; 
+  if (adc_key_in < 650)  return btnSelect;   
 
   return btnNone;
 }
@@ -35,38 +43,46 @@ int readLcdButtons()
 void runStateMachine()
 {
   const int buttonPressed = readLcdButtons();
+  lcd.clear();
   switch(currentState)
   {
     case AvailableStates::ReadTemp:
-    
+    {     
       if(buttonPressed == btnSelect)
       {
         currentState = AvailableStates::SetMinTemp;
         lcd.setCursor(0, 0);
         lcd.print("IMPOSTAZIONE");
-        lcd.setCursor(1, 0);
+        lcd.setCursor(0, 1);
         lcd.print("TEMPERATURA");
         delay(2000); 
       }
       else
-        // Leggi temperatura sonda
+      {
+        lcd.setCursor(0, 0);
+        lcd.print("TEMP CORRENTE:");
+        lcd.setCursor(3, 1);
+        char string[13];
+        sprintf(string, "%d C", currentTemp);
+        lcd.print(string);
+      }
       
     break;
-
+    }
+    
     case AvailableStates::SetMinTemp:
     {
       lcd.setCursor(0, 0);
       lcd.print("MINIMA:");
-      lcd.setCursor(1, 0);
-      char string[16];
-      sprintf(string, "%d °C", minTemp); 
-      lcd.print(string);
-
+      lcd.setCursor(0, 1);
+      lcd.print(minTemp);
+      lcd.print(" C");
+      
       switch (buttonPressed)
       {
         case btnSelect:
             // Salvare valore temperatura minima in memoria
-          currentState = AvailableStates::SetMinTemp;
+          currentState = AvailableStates::SetMaxTemp;
         break;
 
         case btnUp    : minTemp += 1;   break;
@@ -74,6 +90,7 @@ void runStateMachine()
         case btnLeft  : minTemp += 10;  break;
         case btnRight : minTemp -= 10;  break;  
       }
+      
       break;
     }
 
@@ -81,16 +98,15 @@ void runStateMachine()
     {
       lcd.setCursor(0, 0);
       lcd.print("MASSIMA:");
-      lcd.setCursor(1, 0);
-      char string[16];
-      sprintf(string, "%d °C", maxTemp); 
-      lcd.print(string);
+      lcd.setCursor(0, 1);
+      lcd.print(maxTemp);
+      lcd.print(" C");
 
       switch (buttonPressed)
       {
         case btnSelect:
             // Salvare valore temperatura massima in memoria
-          currentState = AvailableStates::SetMaxTemp;
+          currentState = AvailableStates::ReadTemp;
         break;
 
         case btnUp    : maxTemp += 1;   break;
@@ -98,6 +114,7 @@ void runStateMachine()
         case btnLeft  : maxTemp += 10;  break;
         case btnRight : maxTemp -= 10;  break;  
       }
+      
       break;
     }
   }
@@ -105,21 +122,31 @@ void runStateMachine()
 
 void setup() {
   // Caricare valori minimi e massimi temperatura
+  digitalWrite(SS, HIGH);
+
+  Serial.begin(9600); // DEBUG
+  
   lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print("Controllore");
-  lcd.setCursor(1, 0);
-  lcd.print("pizza papà");
+  lcd.setCursor(0, 0);
+  lcd.print("CONTROLLORE");
+  lcd.setCursor(0, 1);
+  lcd.print("PIZZA PAPA'");
   delay(2000);
+  lcd.clear();
 }
 
 void loop() {
-  runStateMachine();
+  currentTemp = ktc.readCelsius();
 
   if(currentTemp < minTemp || currentTemp > maxTemp)
   {
     // Aprire relè
   }
-
-  delay(100);
+  else
+  {
+    // Chiudere Relè
+  }
+  
+  runStateMachine();
+  delay(200);
 }
